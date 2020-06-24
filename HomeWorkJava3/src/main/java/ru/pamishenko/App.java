87fed893f5e2cfdef271bfc7aa280486d7668add
pamishenko <pamishenko@gmail.com>
2020-06-24 17:09:00 +0300

    Create home patch

---
+++ HomeWorkJava3/src/main/java/ru/pamishenko/App.java
@@ -0,0 +1,13 @@
+package ru.pamishenko;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
